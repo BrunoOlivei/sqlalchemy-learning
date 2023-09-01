e28f4333cd68742cf4936927446769efc7d98fb6
Bruno Oliveira <bruoli3@gmail.com>
2023-08-31 21:27:57 -0300

    Working with data

--- code/04.working_with_data.py
+++ code/04.working_with_data.py
@@ -1,5 +1,9 @@
+from typing import List, Optional
+
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy import create_engine, insert, select
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, \
+    relationship
 # from sqlalchemy import select, bindparam
 
 engine = create_engine('sqlite:///:memory:', echo=True)
@@ -26,14 +30,17 @@ address_table = Table(
 metadata_obj.create_all(engine)
 
 
-stmt = insert(user_table).values(name="spongebob",
-                                 fullname="Spongebob Squarepants")
+insert_stmt = insert(user_table).values(name='spongebob',
+                                        fullname='Spongebob Squarepants')
 
-print(stmt)
+# print(stmt)
+# print(insert_stmt.compile().params)
 
-compiled = stmt.compile()
+with engine.connect() as conn:
+    result = conn.execute(insert_stmt)
+    conn.commit()
 
-print(compiled.params)
+# print(compiled.params)
 
 # scalar_subq = (
 #     select(user_table.c.id).
@@ -63,16 +70,67 @@ print(compiled.params)
 
 #     conn.commit()
 
-print(insert(user_table).values().compile(engine))
+# print(insert(user_table).values().compile(engine))
 
 insert_stmt = insert(address_table).returning(
     address_table.c.id, address_table.c.email_address
 )
-print(insert_stmt)
+# print(insert_stmt)
+
+# select_stmt = select(user_table.c.id, user_table.c.name + "@aol.com")
+# insert_stmt = insert(address_table).from_select(
+#     ["user_id", "email_address"], select_stmt
+# )
+# print(insert_stmt.returning(address_table.c.id,
+#                             address_table.c.email_address))
+
+# with Session(engine) as session:
+#     row = session.execute(select(user_table)).first()
+#     print(row[0])
 
 
-select_stmt = select(user_table.c.id, user_table.c.name + "@aol.com")
-insert_stmt = insert(address_table).from_select(
-    ["user_id", "email_address"], select_stmt
-)
-print(insert_stmt.returning(address_table.c.id, address_table.c.email_address))
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[str] = mapped_column(String)
+
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r},\
+              fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email_address: Mapped[str] = mapped_column(String, nullable=False)
+
+    user_id: Mapped[int] = mapped_column(Integer,
+                                         ForeignKey("user_account.id"))
+    user: Mapped[Optional[User]] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+
+insert_stmt = insert(User).values(name='spongebob',
+                                  fullname='Spongebob Squarepants')
+
+with Session(engine) as session:
+    row = session.execute(select(User)).first()
+    print("Print from User table:")
+    print(row)
+
+
+with Session(engine) as session:
+    user = session.scalars(select(User)).first()
+    print("Print from User table:")
+    print(user)
